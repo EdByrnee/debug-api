2a6aac63525af83fa6f4c48898b3d6d2c53fce44
EdByrnee <ed_b@hotmail.co.uk>
2017-03-21 16:24:55 +0000

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+// INDEX
+router.get('/', function(req, res, next) {
+  	console.log("GET / ");
+ 	console.log("Query:" + req.query); 
+ 	res.status(200).json({"message" : "Request logged to console"}).end();
+});
+
+// POST
+router.post('/', function(req, res, next) {
+	console.log("POST /");
+  	console.log("Body: " + JSON.stringify(req.body));
+  	res.status(200).json({"message" : "Request logged to console"}).end();
+});
+
+// UPDATE
+router.put('/:id', function(req, res, next) {
+  	console.log("PUT /");
+  	console.log("Body: " + JSON.stringify(req.body));
+  	res.status(200).json({"message" : "Request logged to console"}).end();
+
+});
+
+// DELETE
+router.delete('/', function(req, res, next) {
+  	console.log("DELETE / ");
+  	res.status(200).json({"message" : "Request logged to console"}).end();
 });
 
 module.exports = router;
+
+ 
